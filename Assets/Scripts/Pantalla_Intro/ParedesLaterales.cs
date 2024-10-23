d6eec8ff8d0d2a54e70a0fb4cc810d61c8e4bd4a
Dani <danielferrert10@gmail.com>
2024-10-23 15:59:20 +0200

    Paredes laterales terminadas

--- Assets/Scripts/Pantalla_Intro/ParedesLaterales.cs
+++ Assets/Scripts/Pantalla_Intro/ParedesLaterales.cs
@@ -6,31 +6,48 @@ public class ParedesLaterales : MonoBehaviour
 {
     public float speed = 5f;
 
+    float cooldown = 2;
+    public float time = 0;
+    public float finalTime;
+
+    public GameObject marcos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        marcos.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movimiento();
+        coolDown();
+
+        if(time >= finalTime)
+        {
+            Movimiento();
+            marcos.SetActive(true);
+        }
     }
 
     private void Movimiento()
     {
-        if(transform.position.x >= -3.3f && gameObject.tag == "Izq")
+        if(transform.position.x >= -.2f && gameObject.tag == "Izq")
         {
-            transform.position = new Vector3(-3.3f, -3.54f, 0);
+            transform.position = new Vector3(-0.2f, 4.39f, 0);
         }
-        else if(transform.position.x <= 3.3f && gameObject.tag == "Der")
+        else if(transform.position.x <= .2f && gameObject.tag == "Der")
         {
-            transform.position = new Vector3(3.3f, -3.54f, 0);
+            transform.position = new Vector3(.2f, 4.39f, 0);
         }
         else
         {
             transform.Translate(Vector3.right * Time.deltaTime * speed);
         }
+    }
+
+    void coolDown()
+    {
+        time = Time.time + cooldown;
     }
 }
