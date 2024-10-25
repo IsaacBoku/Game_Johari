cd2c2b4208e096793de6a7636cab8dd4ce82712d
Dani <danielferrert10@gmail.com>
2024-10-25 15:48:07 +0200

    Placeholders hub y movimiento (sin salto)

---
+++ Assets/Scripts/ScriptsHub/PlayerHub.cs
@@ -0,0 +1,46 @@
+using Autodesk.Fbx;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHub : MonoBehaviour
+{
+    public float speed = 13f;
+    public float jumpForce = 5f;
+    bool isGrounded = true;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimiento();
+    }
+
+    public void Movimiento()
+    {
+        if (isGrounded)
+        {
+            transform.Translate(Vector2.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
