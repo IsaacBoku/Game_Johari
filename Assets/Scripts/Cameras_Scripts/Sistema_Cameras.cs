c068dff1ec71fe7bcf4ed5865c25532088044401
IsaacBoku <visaac762@gmail.com>
2024-11-05 15:29:51 +0100

    Configuracion de camaras - Añadido scrip camara Lobyy

---
+++ Assets/Scripts/Cameras_Scripts/Sistema_Cameras.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sistema_Cameras : MonoBehaviour
+{
+
+    [Header("Cameras")]
+    public GameObject cameraInicial;
+    public GameObject cameraOuticial;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        cameraInicial.SetActive(false);
+        cameraOuticial.SetActive(true);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        cameraInicial.SetActive(true);
+        cameraOuticial.SetActive(false);
+    }
+}
