58447f096a0ebb23c833970f38056fbd5a8cd0cc
Dani <danielferrert10@gmail.com>
2024-12-04 18:38:37 +0100

    musica y sonidos animacion y menu

---
+++ Assets/Scripts/ScriptsHub/AudioActivator.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioActivator : MonoBehaviour
+{
+    public string nombreMusica;
+    public string nombreSFX;
+    public string nombreMusic2 = "MusicaJuego";
+    public float time = 0f;
+    float cooldown = 2f;
+    public float finalTime;
+
+    bool activador = false;
+    //130.8
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.instance.PlayMusic(nombreMusica);
+        AudioManager.instance.PlaySFX(nombreSFX);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CoolDown();
+
+        if(time >= finalTime && activador == false)
+        {
+            AudioManager.instance.PlaySFX(nombreMusic2);
+            activador = true;
+        }
+    }
+
+    void CoolDown()
+    {
+        time = Time.time + cooldown;
+    }
+}
